8c823ae526589be400b8add8634efba4935a0e15
kvalrie <hugo.barcelona31@gmail.com>
2017-05-18 11:06:53 +0200

    Avant bonnus/sans correctif

---
+++ ACC.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="qcm.css">
+<meta charset="UTF-8">
+
+	<title>ACCEUIL</title>
+</head>
+<body>
+
+	
+			
+	<div class="jumbotron">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6 col-md-offset-3"> 
+					  <h1>Le Quiz : culture générale</h1>
+					  <p>Si tu as pas 10/10, tu as raté ta vie. </p>
+					  <p><a class="btn btn-primary btn-lg" href="QCM.php" role="button" >J'ose</a></p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+<img src="79a.gif" class="img-responsive center-block text-center">
+</body>
+</html>
